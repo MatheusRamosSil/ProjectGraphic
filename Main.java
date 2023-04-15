efa555dc85c0eb820c8e6d8e350bc2313e4962df
Matheus Ramos <matheusjramos2000@gmail.com>
2023-04-15 17:29:46 -0300

    Subindo projeto

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Graphic g = new Graphic();
-        Vertices s = g.addVertice("s");
-        Vertices t = g.addVertice("t");
-        Vertices y = g.addVertice("y");
-        Edges st = g.addAresta(s, t,12);
-        Edges sy = g.addAresta(s, y,0);
-        Edges ty = g.addAresta(t, y,1);
-        Edges yt = g.addAresta(y, t,10);
-        System.out.println(g);
+      
+        GrafoNaoOrientado grafo = new GrafoNaoOrientado(5);
+        grafo.adicionarVertice("PB");
+        grafo.adicionarVertice("PE");
+        grafo.adicionarVertice("RN");
+        grafo.adicionarVertice("BA");
+        grafo.adicionarVertice("MA");
+
+        grafo.adicionarAresta("PB", "PE",120000);
+        
+        grafo.adicionarAresta("PB", "RN",80000);
+        grafo.adicionarAresta("BA", "MA",200000);
+        grafo.adicionarAresta("PE", "PB",400000);
+        grafo.adicionarAresta("PB", "BA",300);
+        
+
+        grafo.imprimirMatrizAdj();
     }
 }
